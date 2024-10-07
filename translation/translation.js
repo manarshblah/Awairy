8822844359a954407821e4e73b8f21698b5d1af0
unknown <manarshblah@gmail.com>
2024-10-07 14:15:41 +0300

    first commit

---
+++ translation/translation.js
@@ -0,0 +1,197 @@
+
+const translations = {
+    en: {
+      'header.logo-title': 'AlAwa<span class="span">iry-Ladies',
+      'header.title':'AlAwairy-Ladies',
+      'header.home': 'Home',
+      'header.category': 'Category',
+      'header.about': 'About Us',
+      'slider.ourSpecials':'our specials',
+      'slider.fashionTrend':'fashion trend',
+      'slider.about':'about us',
+      'slider.fashionStyle':' fashion style',
+      'slider.goNow':'go now',
+      'slider.women':'women',
+      'women':'all what you need',
+      'slider.girl':'girls',
+      'girls':'all new brand fashion you can get from us',
+      'slider.kids':'kids',
+      'kids':'have a comfortable clothes for your little girls',
+      'slider.lingerie':'lingerie',
+      'TrendingProducts':'Trending Products',
+      'Featured':'Featured',
+      'Latest':'Latest',
+      'Bestseller':'Bestseller',
+      'dis1':'black and white dress ',
+      'dis2':'black dress with textured',
+      'dis3':'White dress with textured',
+      'dis4':'Pink skirt swirl',
+      'dis5':'gray dress with black details.',
+      'dis6':'Grey wool dress',
+      'dis7':'White dress',
+      'dis8':'Classic black dress',
+      'dis9':'pink embellished dress',
+      'dis10':'Black jacket with red sweater',
+      'dis11':'Black and white striped shawl',
+      'dis12':'Winter clothes',
+      'dis13':'Black winter jacket',
+      'dis14':'Yellow pajama',
+      'dis15':'white dress',
+      'dis16':'Nice set',
+      'dis17':'Red shirt and jeans',
+      'dis18':'Green jacket with white striped shirt',
+      'dis19':'Blue shirt with black pants',
+      'dis20':'Pink wool jacket',
+      'dis21':'white wool jackei',
+      'dis.model':'📸 When life gives you a chance to shine, strike a pose and own the spotlight! Embrace the power of simplicity with these versatile dresses. Pictures are worth a thousand words',
+       'Size':'size',
+       'Medium':'Medium',
+       'large':'large',
+       'small':'small',
+       'ExtraLarge':'Extra Large',
+       'color':'color',
+       'qty':'qty',
+       'AddToCart':' Add To Cart',
+       'storeinformation':'store information',
+       'dubai,deira':'dubai, deira',
+       'summershop':'summer shop',
+      'upto70%off':'up to 70% off',
+      'shopnow':'shop now',
+      'witershop':'witer shop',
+      'alert':'<b>Do you like cookies?</b> We use cookies to ensure you get the best  experience on our website  <a href="https://cookiesandyou.com/">Learn more</a> <button type="button" class="btn btn-primary btn-sm acceptcookies" aria-label="Close">   I agree</button>',
+      //category
+      'ttheader-service':'Wants to explore Upcoming Deals on Weekends?',
+      'English':'English',
+      'arabic':'arabic',
+      'showpages':'Showing 1 to 8 of 16 (3 Pages)',
+      'modal-title':'Product successfully added to your shopping cart',
+      'compareproducts':'compare products',
+      'showall':'ahow all',
+      'Categories':'Categories',
+       'Jackets':'Jackets',
+       'Jeans&chinos':'Jeans &amp; chinos',
+       'price':'price',
+       'Black':'Black',
+       'Blue':'Blue',
+       'Brown':'Brown',
+       'White':'White',
+       'saleproducts':'sale products',
+       'Dresses':'Dresses',
+       'lingerie':'lingerie',
+       'kids':'kids',
+       // Add more translations here
+    },
+    ar: {
+      'header.logo-title': 'العويري <span class="span">للألبسة النسائية',
+      'header.title':'العويري للألبسة النسائية',
+      'header.home': 'الصفحة الرئيسية',
+      'header.category': 'الفئة',
+      'header.about': 'من نحن',
+      'slider.ourSpecials':'عروضنا الخاصة',
+      'slider.fashionTrend':'الموضة العصرية',
+      'slider.about':'من نحن',
+      'slider.fashionStyle':'نمط الموضة',
+      'slider.goNow':'اذهب الان',
+      'slider.women':'النساء',
+      'women':'كل ما تحاجين',
+      'slider.girl':'الفتيات',
+      'girls':'كل أزياء العلامة التجارية الجديدة التي يمكنك الحصول عليها منا',
+      'slider.kids':'الفتيات الصغيرات',
+      'kids':'احصل على ملابس مريحة لفتياتك الصغيرات.   ',
+      'slider.lingerie':'لانجيري',
+      'TrendingProducts':'المنتجات الشائعة',
+      'Featured':'المتميز',
+      'Latest':'الاخير',
+      'Bestseller':'الاكثر مبيعا',
+      'dis1':'فستان ابيض واسود ',
+      'dis2':'فستان اسود مزركش',
+      'dis3':'فستان ابيض مزركش',
+      'dis4':'تنورة وردية',
+      'dis5':'فستان رمادي مع تفاصيل سوداء',
+      'dis6':'فستان رمادي من الصوف',
+      'dis7':'فستان ابيض',
+      'dis8':'فستان اسود كلاسيك',
+      'dis9':'فستان وردي مزركش',
+      'dis10':'جاكيت اسود مع كنزة حمراء',
+      'dis11':'سروال مخطط ابيض واسود',
+      'dis12':'ملابس شتوية',
+      'dis13':'جاكيت شتوية سوداء',
+      'dis14':'بجامة صفراء',
+      'dis15':'فستان ابيض',
+      'dis16':'طقم جميل',
+      'dis17':'قميص احمر وسروال جينز',
+      'dis18':'جاكيت خضراء مع قميص ابيض مخطط',
+      'dis19':'قميص ازرق مع سروال اسود',
+      'dis20':'جاكيت صوف وردية',
+      'dis21':'جاكيت صوف بيضاء',
+      'dis.model':'📸 عندما تمنحك الحياة فرصة للتألق ، اضرب وقفة وامتلك الأضواء!احتضن قوة البساطة مع هذا الفساتين متعدد الاستخدامات. الصور تساوي الف كلمة',
+      'Size':'القياس',
+      'Medium':'ميديوم',
+      'large':'لارج',
+      'small':'سمول',
+      'ExtraLarge':'اكسترا لارج',
+      'color':'اللون',
+      'qty':'الكمية',
+      'AddToCart':' اضافة الى السلة',
+      'storeinformation':'معلومات المتجر',
+      'dubai,deira':'دبي, ديرة',
+      'summershop':'متجر الصيف',
+      'upto70%off':'خصم حتى 70٪',
+      'shopnow':'اشتري الان',
+      'witershop':'متجر الشتاء',
+      'alert':'<b>هل تحب ملفات تعريف الارتباط؟</b> نحن نستخدم ملفات تعريف الارتباط لضمان حصولك على أفضل تجربة على موقعنا  <a href="https://cookiesandyou.com/">معرفة المزيد</a> <button type="button" class="btn btn-primary btn-sm acceptcookies" aria-label="Close">  موافق</button>',
+      //category
+      'ttheader-service':'هل تريد استكشاف الصفقات القادمة في عطلات نهاية الأسبوع؟',
+      'English':'الانجليزية',
+      'arabic':'العربية',
+      'showpages':'عرض 1 الى 8 من 16 (3 صفحات)',
+      'modal-title':'تم اضافة المنتج بنجاح الى سلةالمشتريات',
+      'compareproducts':'مقارنة المنتجات',
+      'showall':'اظهار الجميع',
+      'Categories':'الفئات',
+      'Jackets':'الستر',
+      'Jeans&chinos':'جينز وبنطال تشينو',
+      'price':'السعر',
+      'Black':'اسود',
+      'Blue':'ازرق',
+      'Brown':'بني',
+      'White':'ابيض',
+      'saleproducts':'التخفيضات',
+      'Dresses':'فساتين',
+      'lingerie':'لانجيري',
+      'kids':'اطفال',
+      // Add more translations here
+    },
+  };
+  
+  const defaultLanguage = 'en'; // Set the default language
+  let currentLanguage = defaultLanguage; // Initialize the current language
+  
+
+function translatePage() {
+    const elements = document.querySelectorAll('[data-translate]');
+    elements.forEach((element) => {
+      const translationKey = element.getAttribute('data-translate');
+      element.innerHTML = translations[currentLanguage][translationKey];
+    });
+  }
+  
+
+
+function changeLanguage(language) {
+    currentLanguage = language;
+    translatePage();
+  }
+  
+  // Example of how to change the language:
+  document.getElementById('english-button').addEventListener('click', () => {
+    changeLanguage('en');
+  });
+  
+  document.getElementById('arabic-button').addEventListener('click', () => {
+    changeLanguage('ar');
+  });
+  
+  // Initially translate the page to the default language
+  translatePage();
+  
